ffa5ccf1f96229da731b3e5282906e2d05635a58
BrayanMajin <bmajin@unicauca.edu.co>
2025-03-30 00:48:40 -0500

    refactor HomeScreenPreview

--- app/src/main/java/edu/unicauca/lookapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/edu/unicauca/lookapp/ui/screen/home/HomeScreen.kt
@@ -7,13 +7,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.unicauca.lookapp.R
 import edu.unicauca.lookapp.ui.components.BottomNavBar
 import edu.unicauca.lookapp.ui.components.TopAppBar
-import edu.unicauca.lookapp.ui.theme.LookAppTheme
 
 @Composable
 fun Home(
@@ -28,20 +26,6 @@ fun Home(
 
 }
 
-@Composable
-fun HomeScreen(){
-    LookAppTheme{
-        Scaffold (
-           // topBar = { TopAppBar(title = R.string.title_Home)},
-            bottomBar = { BottomNavBar() }
-
-        ){ padding ->
-            Home(Modifier.padding(padding))
-
-        }
-    }
-
-}
 
 @Preview
 @Composable
@@ -52,5 +36,12 @@ fun HomePreview(){
 @Preview(showBackground = true, locale = "es")
 @Composable
 fun HomeScreenPreview(){
-    HomeScreen()
+    Scaffold (
+       // topBar = { TopAppBar(title = R.string.title_Home)},
+        bottomBar = { BottomNavBar() }
+
+    ){ padding ->
+        Home(Modifier.padding(padding))
+
+    }
 }
